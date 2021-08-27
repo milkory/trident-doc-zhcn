1cd8d754143d83ce2fbf7e797a8aa7e002e3d328
APJifengc <mcbigshen@qq.com>
2021-08-28 06:33:01 +0800

    Tdn highlight

--- docs/assets/js/prism-tdn.js
+++ docs/assets/js/prism-tdn.js
@@ -1,6 +1,6 @@
 Prism.languages.tdn = {
   comment: {
-    pattern: /^#.*/,
+    pattern: /(^|[^\\:])#.*/,
     lookbehind: true,
     greedy: true
   },
@@ -16,13 +16,41 @@ Prism.languages.tdn = {
       punctuation: /[\[\]\,]/
     }
   },
-  punctuation: /[\[\{\}\]\,:]/,
-  number: /\s/,
+  punctuation: /[\[\{\}\]\,]/,
   string: {
     pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
     greedy: true
   },
-  keyword: {
-    pattern: /^(execute)/
+  important: { // TODO: keywords
+    pattern: /(execute|say|summon)/
+  },
+  keyword: [
+    {
+      pattern: /\b(as|at|run|store|result|score|if|entity)\b/
+    },
+    {
+      pattern: /\balign\s(xyz|xy|yz|xz|x|y|z)\b/,
+      inside: {
+        statement: /(xyz|xy|yz|xz|x|y|z)/
+      }
+    }
+  ],
+  placeholder: /(armor_stand)/,
+  number: {
+    pattern: /[+-]?(([0-9]+)(\.[0-9]+)?|(\.[0-9]+))[bdsl]?/,
+  },
+  coordinate: {
+    pattern: /(\^|~)[+-]?([0-9]*)(\.[0-9]+)?/,
+    alias: "number"
+  },
+  key: {
+    pattern: /\w[a-zA-Z_]+\s*:/,
+    alias: "variable",
+    inside: {
+      punctuation: /:/
+    }
+  },
+  control: {
+    pattern: /(define)/
   }
 }
